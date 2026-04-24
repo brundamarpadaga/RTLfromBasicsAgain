48e14c4def9e2091c4d5fc952e7aef3942e8bf62
marpa <brundamarpadaga@gmail.com>
2026-04-24 04:52:36 +0000

    productive environment for SV practise

---
+++ counter.sv
@@ -0,0 +1,19 @@
+module counter #(
+    parameter int WIDTH = 8
+) (
+    input logic clk,
+    input logic rst,
+    input logic en,
+    output logic [WIDTH-1:0] count
+);
+  always_ff @(posedge clk, negedge rst) begin
+    if (!rst) begin
+      count <= 0;
+    end else if (en) begin
+      count <= count + 1;
+    end
+
+  end
+
+endmodule
+
